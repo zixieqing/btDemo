c19f91dede6c83f52b60a4f460341df0b9a764d2
yinchuanfu <yinchuanfu@yofijoy.com>
2024-08-06 10:03:03 +0800

    feat:挖洞

---
+++ src/app/effect/HoleEffect.lua
@@ -0,0 +1,53 @@
+
+-- 挖洞
+---@class HoleEffect
+
+local test_sp = "huli.png"
+
+local shader = {
+    fs = require "app.shader.HoleFS",
+    vert = require "app.shader.NormalVS",
+}
+
+
+local HoleEffect = class("HoleEffect")
+
+function HoleEffect.getDescText()
+    return "挖洞"
+end
+
+function HoleEffect:init(node)
+    node:spTouchUseShader(test_sp, "hole_effect")
+end
+
+function HoleEffect:setUniform(glState, time, node)
+    local u_holeCenter = cc.p(0.5, 0.5)
+
+    glState:setUniformVec2("u_holeCenter", u_holeCenter)
+    glState:setUniformFloat("u_holeRadius", 0.1)
+
+    local tex = node:getTexture()
+    glState:setUniformTexture("u_texture", tex)
+end
+
+function HoleEffect:resetUniform()
+end
+
+function HoleEffect.getVert()
+    return shader.vert
+end
+
+function HoleEffect.needRefresh()
+    return true
+end
+
+function HoleEffect.getFS()
+    return shader.fs
+end
+
+function HoleEffect:resetTime(time)
+    return time + 0.008
+end
+
+return HoleEffect
+
---
+++ src/app/shader/HoleFS.lua
@@ -0,0 +1,23 @@
+return [[
+precision mediump float;
+varying vec2 v_texCoord;
+uniform sampler2D u_texture;
+uniform vec2 u_holeCenter; // 挖洞中心
+uniform float u_holeRadius; // 挖洞半径
+
+void main()
+{
+    vec4 color = texture2D(u_texture, v_texCoord);
+    
+    // 计算当前像素到挖洞中心的距离
+    float dist = distance(v_texCoord, u_holeCenter);
+    
+    // 如果在挖洞范围内，返回透明色
+    if (dist < u_holeRadius) {
+        discard; // 丢弃该片段
+    }
+    
+    gl_FragColor = color; // 否则显示原始颜色
+}
+
+]]
